2a10230632f4083647b55cf6cbe33154e427a90c
joseima <joseima17@gmail.com>
2024-01-28 11:51:19 +0100

    added API consult and shown via handled with useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 
 import { Button, Col } from 'antd';
 import Searcher from './components/Searcher';
-import './App.css';
 import PokemonList from './components/PokemonList';
+import logo from './statics/logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import { getPokemon } from './api';
 
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(()=> {
+    const fetchPokemons = async () => {
+      const pokemonsRes = await getPokemon();
+      setPokemons(pokemonsRes);
+    };
+    fetchPokemons();
+  },[]);
+
+
   return (
     <>
+    <Col span={4} offset={10}>
+      <img  className='pokeLogo' src={logo} alt='Pokedux' />
+    </Col>
     <Col span={8} offset={8}>
       <Searcher />
       <Button type="primary">Button</Button>
     </Col>
-    <PokemonList />
+    <PokemonList pokemons={pokemons} />
     </>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,11 +2,13 @@ import { StarOutlined } from '@ant-design/icons';
 import {Card} from 'antd';
 import Meta from 'antd/es/card/Meta';
 
-const PokemonCard =() => {
+const PokemonCard =({name}) => {
     return (
     <Card  
-        title="Ditto" 
-        cover={<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' alt='Ditto'/>}
+        title={name} 
+        cover={<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' 
+        alt='Ditto'
+    />}
         extra={<StarOutlined/>}    
     >
         <Meta description="fire, magic" />
